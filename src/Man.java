fff0a178419c6936f5cb173a6389e9cbdcfb21cd
andreasTest <nikolakopoulosand@gmail.com>
2015-10-16 14:45:57 +0200

    Ajout fonction Buzz

--- src/Man.java
+++ src/Man.java
@@ -11,7 +11,12 @@ public class Man {
 	
 	public static void Bizz (int n){
 		
+		System.out.println("multiple de 3");
+	}
+	
+public static void Buzz (int n){
 		
+	System.out.println("multiple de 5");
 	}
 
 }
